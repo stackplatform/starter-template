4e3b033466d26a3b2df270637734113b44cd25fe
Clay Larabie <clay@alumind.com.au>
2026-04-02 07:30:47 +1100

    fix: update starter-template launcher to use Client.exe instead of hl.exe

--- client/run-with-update.bat
+++ client/run-with-update.bat
@@ -2,12 +2,12 @@
 REM Run the Client with the Automatic Updater (HashLink)
 cd /d "%~dp0"
 
-if not exist "Export\hl\bin\hl.exe" (
+if not exist "Export\hl\bin\Client.exe" (
     echo Error: Client has not been built yet. Run 'lime build hl' first.
     pause
     exit /b
 )
 
 cd Export\hl\bin
-.\hl.exe updater.hl
+.\Client.exe updater.hl
 pause
